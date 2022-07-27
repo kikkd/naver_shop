781f8cebb6349ea4458e8facb34fe76626a939ea
kikkd <whddls6666@naver.com>
2022-07-28 08:54:35 +0900

    제조사 찾기

--- 5.구매하기_다나와/2.py
+++ 5.구매하기_다나와/2.py
@@ -24,6 +24,19 @@ def finds_visible(CSS):
     find_visible(CSS)
     return chrome.find_elements(By.CSS_SELECTOR,CSS)
 
+### 페이지 iframe 스위치 함수
+def right_frame():
+    chrome.switch_to.parent_frame() # 부모 프레임으로 이동
+    find_visible("iframe#ifrmWish")
+    chrome.switch_to.frame("ifrmWish") # 이전 프레임 사용 시절 스위치 방법
+
+def left_frame():
+    chrome.switch_to.parent_frame() # 부모 프레임으로 이동
+    find_visible("iframe#ifrmProduct")
+    chrome.switch_to.frame("ifrmProduct")
+
+
+
 # cpu, 메인보드, 메모리, 그래픽카드, ssd, 케이스, 파워
 # cpu 한번 클릭
 
@@ -45,9 +58,8 @@ print(Category_css)
 
 chrome.get("https://shop.danawa.com/virtualestimate/?controller=estimateMain&methods=index&marketPlaceSeq=16")
 
-# find_visible("iframe#ifrmWish")
-# chrome.switch_to.frame("ifrmWish") # 이전 프레임 사용 시절 스위치 방법
-
+###
+# right_frame():
 # CPU 카테고리 클릭
 find_visible(Category_css["CPU"]).click()
 # cpu = find_visible("dd.category_"+Category["메인보드"]+" a") # dd안에 있는 a를 선택
@@ -57,13 +69,18 @@ find_visible(Category_css["CPU"]).click()
 time.sleep(1)
 
 # CPU 제조사 불러오기
+###
+# left_frame()
 options = finds_visible("div[class=search_option_list] div[class=search_option_item]:nth-child(2) label[class=item_checkbox] span[class=item_text]")
 more_options = finds_visible("div[class=search_option_list] div[class=search_option_item] div[class=search_cate_contents] button[class=btn_item_more]")
 more_options[0].click()
 options[10].click()
-
-for o in options:
-    print(o.text)
+print("CPU 제조사를 골라주세요")
+for i in range(len(options)):
+    print(str(i+1)+". "+options[i].text)
+    # options[i]
+# for o in options:
+#     print(o.text)
 
 
 
